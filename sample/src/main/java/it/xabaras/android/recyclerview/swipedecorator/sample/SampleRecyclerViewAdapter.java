7be8a0f390171b9c0357c29eb4da6ef874586c8d
Paolo Montalto <p.montalto@twomenstudio.com>
2021-07-28 12:20:10 +0200

    Migrate to androidx namespace

--- sample/src/main/java/it/xabaras/android/recyclerview/swipedecorator/sample/SampleRecyclerViewAdapter.java
+++ sample/src/main/java/it/xabaras/android/recyclerview/swipedecorator/sample/SampleRecyclerViewAdapter.java
@@ -1,7 +1,7 @@
 package it.xabaras.android.recyclerview.swipedecorator.sample;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
